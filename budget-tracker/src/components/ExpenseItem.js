eec39e644954e94c6ffe42ab1b3155651e79ea20
jenn-doyle <jenndoyle@hotmail.co.uk>
2021-05-30 16:24:41 +0100

    updated props

--- budget-tracker/src/components/ExpenseItem.js
+++ budget-tracker/src/components/ExpenseItem.js
@@ -2,22 +2,22 @@ import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
-const ExpenseItem = (props) => {
+const ExpenseItem = ({id, name, cost}) => {
 	const { dispatch } = useContext(AppContext);
 
 	const handleDeleteExpense = () => {
 		dispatch({
 			type: 'DELETE_EXPENSE',
-			payload: props.id,
+			payload: id,
 		});
 	};
 
 	return (
 		<li className='list-group-item d-flex justify-content-between align-items-center'>
-			{props.name}
+			{name}
 			<div>
 				<span className='badge badge-primary badge-pill mr-3'>
-					£{props.cost}
+					£{cost}
 				</span>
 				<TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete>
 			</div>
